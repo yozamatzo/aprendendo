f48ac66a67d77fde8b2bfc243903f0bb76b107a9
yozamatzo <yorimartins26@gmail.com>
2025-04-26 16:23:37 -0300

    feat:contador de 1 a 100

---
+++ src/contador.java
@@ -0,0 +1,15 @@
+public class contador {
+    public static void main(String[] args)
+    {
+      int i = 1;
+
+      for(i = 1; i <= 100; i++)
+      {
+
+        System.out.println(i);
+
+      }
+
+    }
+
+}
